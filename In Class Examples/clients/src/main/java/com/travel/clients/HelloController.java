f5908b32bb9abfabcf98a831e222fd309d6e7505
Judah319n <w0509673@nscc.ca>
2025-09-26 15:16:25 -0300

    login

--- In Class Examples/clients/src/main/java/com/travel/clients/HelloController.java
+++ In Class Examples/clients/src/main/java/com/travel/clients/HelloController.java
@@ -7,18 +7,15 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/client")
     public String getHello(Model model) {
-        ArrayList<String> clients = new ArrayList<String>();
-        clients.add("Nadia");
-        clients.add("Esraa");
-        clients.add("Josh");
-        clients.add("Jeremy");
-        clients.add("Judah the Best");
 
         ArrayList<User> Users = new ArrayList<User>();
         Users.add(new User("Judah", "P@ssword1"));
@@ -29,9 +26,25 @@ public class HelloController {
 
         model.addAttribute("msg", "List of the Clients");
         model.addAttribute("date", LocalDate.now());
-        model.addAttribute("clientsData", clients);
         model.addAttribute("Users", Users);
         return "client";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            Model model) {
+
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+
+        return "welcome";
     }
 
 }
